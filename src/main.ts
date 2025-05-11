49da3efab312bfbb9b4bd63a9e147e466a129ea0
bbimabluu <bbimabluu@gmail.com>
2025-05-11 23:24:27 +0700

    fix: conditionally load dotenv in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import 'dotenv/config';
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {
