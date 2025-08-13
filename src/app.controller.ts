fe8633ce799d0f9840f43343ab0c918da837a6b7
nadhir24 <nadhirghassan456@gmail.com>
2025-08-13 17:04:24 +0700

    fix: add updatedAt to product_imagess

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello('h'): string {
+  @Get('h')
+  getHello(): string {
     return this.appService.getHello();
   }
   @Get('success')
